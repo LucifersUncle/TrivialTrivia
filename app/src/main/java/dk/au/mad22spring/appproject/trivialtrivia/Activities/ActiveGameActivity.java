7579b28c835ddcb20e99b4839d0b03d752dd7902
GustavSorensen <gustav@fiberflex.dk>
2022-05-02 10:14:07 +0200

    Setup some views
    Har haft nogle Gradle problmer, så prøver lige at commit

---
+++ app/src/main/java/dk/au/mad22spring/appproject/trivialtrivia/Activities/ActiveGameActivity.java
@@ -0,0 +1,16 @@
+package dk.au.mad22spring.appproject.trivialtrivia.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import dk.au.mad22spring.appproject.trivialtrivia.R;
+
+public class ActiveGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_active_game);
+    }
+}
