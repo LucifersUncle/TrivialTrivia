eb1778815b015021b271c00bcd2e9b8e0a6b5a61
LucifersUncle <201801604@post.au.dk>
2022-05-04 15:05:45 +0200

    added some more to the host logic, still needs to handle the game state

--- app/src/main/java/dk/au/mad22spring/appproject/trivialtrivia/Models/Game.java
+++ app/src/main/java/dk/au/mad22spring/appproject/trivialtrivia/Models/Game.java
@@ -1,15 +1,15 @@
 package dk.au.mad22spring.appproject.trivialtrivia.Models;
 
 import java.util.HashMap;
+import java.util.UUID;
 
 public class Game {
-    private String gameName;
-
+    private String gameName, category, difficulty;
     private String documentName;
-
     private int timePerRound, numberOfRounds, currentRound;
 
     private HashMap<String, Player> players;
+
     //game states
     private boolean gameIsActive, gameIsStarted;
 
@@ -17,14 +17,22 @@ public class Game {
         //empty constructor to access object methods
     }
 
-    public Game(String gameName, int timePerRound, int numberOfRounds, String hostName, String documentName, boolean gameIsActive, boolean gameIsStarted) {
+    public Game(String gameName, int timePerRound, int numberOfRounds, String hostName, String documentName, boolean gameIsActive, boolean gameIsStarted, String category, String difficulty) {
         this.gameName = gameName;
         this.timePerRound = timePerRound;
         this.numberOfRounds = numberOfRounds;
         this.currentRound = 1;
-        this.documentName = documentName;
         this.gameIsActive = gameIsActive;
         this.gameIsStarted = gameIsStarted;
+        this.category = category;
+        this.difficulty = difficulty;
+
+        String playerId = UUID.randomUUID().toString();
+
+        Player playerHost = new Player(playerId, "host", hostName,0);
+
+        this.players = new HashMap<>();
+        players.put(playerId, playerHost);
     }
 
     public void setNumberOfRounds(int numberOfRounds) {
@@ -55,11 +63,39 @@ public class Game {
         return currentRound;
     }
 
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public String getCategory() {
+        return category;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public boolean gameIsActive() {
+        return gameIsActive;
+    }
+    public void setGameIsActive(boolean active) {
+        gameIsActive = active;
+    }
+
+    public boolean gameIsStarted() {
+        return gameIsStarted;
+    }
+    public void setGameIsStarted(boolean started) {
+        gameIsStarted = started;
+    }
+
     public String getDocumentName() {
         return documentName;
     }
-
     public void setDocumentName(String documentName) {
         this.documentName = documentName;
     }
+
 }
